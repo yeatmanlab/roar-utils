2c16944425e6cea61d1319feb4fa50496e05fe06
Mohammed Shoaib <shoaib98libra@gmail.com>
2022-11-21 21:48:46 -0800

    Remove usage of typescript and add assets for auditory feedback

--- index.ts
+++
@@ -1,4 +0,0 @@
-export const CORRECT_KEY_PRESS = "ArrowLeft";
-export const CORRECT_KEY_TEXT = "left arrow key";
-export const WRONG_KEY_PRESS = "ArrowRight";
-export const WRONG_KEY_TEXT = "right arrow key";
