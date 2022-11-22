fd177e7605d105b590e4ca9407677a7a147c7e0c
Mohammed Shoaib <shoaib98libra@gmail.com>
2022-11-21 21:51:56 -0800

    Add functionality for auditory feedback and option to preload audio files

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// global modules
+import jsPsychPreload from '@jspsych/plugin-preload';
+
+// assets
+import feedbackCorrect from "../assets/audio/feedbackCorrect.mp3";
+import feedbackIncorrect from "../assets/audio/feedbackIncorrect.mp3";
+
+// local modules
+import { camelizeFiles } from "./utils";
+
+// constants
+export const CORRECT_KEY_PRESS = "ArrowLeft";
+export const CORRECT_KEY_TEXT = "left arrow key";
+export const WRONG_KEY_PRESS = "ArrowRight";
+export const WRONG_KEY_TEXT = "right arrow key";
+
+const audioFiles = [feedbackCorrect, feedbackIncorrect];
+export const audioContent = camelizeFiles(audioFiles);
+export const preloadAudio = {
+  type: jsPsychPreload,
+  audio: audioFiles
+};
---
+++ src/utils.js
@@ -0,0 +1,21 @@
+import path from 'path';
+
+// converts a string to camel case
+export function camelize(string) {
+  return string.replace(
+    /_([a-z])/g,
+    (groups) => groups[1].toUpperCase()
+  );
+}
+
+// get an object where the key is camelize(filename) and the value is the filename
+export function camelizeFiles(files) {
+  let obj = {};
+  
+  for (const filePath of files) {
+    const fileName = path.parse(filePath).name;
+    obj[camelize(fileName)] = filePath;
+  }
+  
+  return obj;
+};
