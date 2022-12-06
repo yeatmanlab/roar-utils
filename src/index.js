d0b5447c7786e62f835b6987588cb46805025d4e
Mohammed Shoaib <shoaib98libra@gmail.com>
2022-12-06 11:58:22 -0800

    Add audio for neutral feedback

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import jsPsychPreload from '@jspsych/plugin-preload';
 // assets
 import feedbackCorrect from '../assets/audio/feedbackCorrect.mp3';
 import feedbackIncorrect from '../assets/audio/feedbackIncorrect.mp3';
+import feedbackNeutral from '../assets/audio/feedbackNeutral.mp3';
 
 // local modules
 import { camelizeFiles } from './utils';
@@ -14,7 +15,11 @@ export const CORRECT_KEY_TEXT = 'left arrow key';
 export const WRONG_KEY_PRESS = 'ArrowRight';
 export const WRONG_KEY_TEXT = 'right arrow key';
 
-const audioFiles = [feedbackCorrect, feedbackIncorrect];
+const audioFiles = [
+  feedbackCorrect,
+  feedbackIncorrect,
+  feedbackNeutral
+];
 export const audioContent = camelizeFiles(audioFiles);
 export const preloadAudio = {
   type: jsPsychPreload,
