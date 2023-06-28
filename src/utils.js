94eab096a8cdb0a10c87352b49f847705fcb746a
Philip Ziolkowski <philipz4848@gmail.com>
2023-06-27 17:30:37 -0700

    Rewriting helper functions to take in JSON file with flat structure. Adding detect-it and mime-types package

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,8 @@
 import path from 'path-browserify';
-// for local testing
-import fs from 'fs'
+import mime from 'mime-types';
+import jsPsychPreload from '@jspsych/plugin-preload';
+import { deviceType, primaryInput } from 'detect-it';
+
 
 
 
@@ -24,121 +26,88 @@ export function camelizeFiles(files) {
   return obj;
 }
 
-/*
-Plays the correct audio based on user response. If user response does not matter, it will play a neutral feedback audio. Takes in 3 parameters:
-- responseIsCorrect (boolean || null): if the user's response was correct/incorrect OR null if it does matter, ex. true OR null
-- correct/neutral audio (string): path to the audio file, ex. "http://localhost:8080/audio/feedbackAudio.mp3"
-- incorrect audio (string): path to the audio file 
-*/
-
-/*
-Examples of the function can be called
-
-playFeedbackAudio(true, 'correct.mp3', 'incorrect.mp3')
-playFeedbackAudio(null, 'neutral.mp3',)
-*/
-
-
-export function playFeedbackAudio(responseIsCorrect, audio1, audio2) {
-    let audioToPlay
-
-    if (responseIsCorrect || responseIsCorrect === null) {
-      audioToPlay = audio1
-    } else {
-      audioToPlay = audio2
-    }
-
-    new Audio(audioToPlay).play()
-}
 // For testing function locally
 let assetStructure
 
-try {
-  const rawData = fs.readFileSync('./assetStructure.json');
-  assetStructure = JSON.parse(rawData);
-
-  // generateAssetObject(assetStructure, 'google.com', 'en', 'keyboard')
-  createPreloadTrials(assetStructure, 'google.com', 'en', 'keyboard')
-} catch (error) {
-  console.error('Error reading JSON file:', error);
+  return decodeURI(results[1]) || 0;
 }
-//
 
-export function generateAssetObject(json, bucketUri, lng, device) {
-  const assetTypes = ["images", "audio", "video"];
-  let assets = {};
+function getDevice() {
+  if (deviceType === 'touchOnly' || (deviceType === 'hybrid' && primaryInput === 'touch')) {
+    return 'mobile'
+  }
+  return 'keyboard'
+}
+
+
+export function generateAssetObject(json, bucketUri) {
+  let assets = { images: {}, video: {}, audio: {} };
+  const lng = getLanguage();
+  const device = getDevice();
+
+  const getAssetType = (filePath) => {
+    const mimeType = mime.lookup(filePath);
+    if (!mimeType) {
+      throw new Error(`Unrecognized file extension in path: ${filePath}`);
+    }
+    if (mimeType.startsWith('image/')) return 'images';
+    if (mimeType.startsWith('video/')) return 'video';
+    if (mimeType.startsWith('audio/')) return 'audio';
+    throw new Error(`Unsupported MIME type for file: ${filePath}. Only image, video, and audio files are supported.`);
+  }
 
   const handleAssets = (obj, type) => {
-    obj[type].languageSpecific.forEach((filePath) => {
+    obj[type].forEach((filePath) => {
+      const assetType = getAssetType(filePath);
       const parsedFileName = path.parse(filePath).name;
-      const camelizedFileName = camelize(parsedFileName)
-
-      const formattedValue = `${bucketUri}/${lng}/${device}/${filePath}`;
-      
-      if (!assets[type]) {
-        assets[type] = {};
-      }
-
-      assets[type][camelizedFileName] = formattedValue;
+      const camelizedFileName = camelize(parsedFileName);
+      const formattedValue = `${bucketUri}/${type === 'languageSpecific' ? lng : 'shared'}/${device}/${filePath}`;
+      assets[assetType][camelizedFileName] = formattedValue;
     });
+  };
 
-    obj[type].shared.forEach((filePath) => {
-      const parsedFileName = path.parse(filePath).name;
-      const camelizedFileName = camelize(parsedFileName)
-
-      const formattedValue = `${bucketUri}/shared/${device}/${filePath}`;
-      
-      if (!assets[type]) {
-        assets[type] = {};
-      }
-
-      assets[type][camelizedFileName] = formattedValue;
+  const handleGroupAssets = (groupObject) => {
+    ['languageSpecific', 'shared'].forEach((type) => {
+      handleAssets(groupObject, type);
     });
-
   };
 
   if (json.preload) {
-    Object.values(json.preload).forEach((preloadObject) => {
-      assetTypes.forEach((type) => {
-        handleAssets(preloadObject, type);
-      });
+    Object.values(json.preload).forEach((groupObject) => {
+      handleGroupAssets(groupObject);
     });
   } else if (json.all) {
-    assetTypes.forEach((type) => {
-      handleAssets(json.all, type);
-    });
+    handleGroupAssets(json.all);
   }
 
   return assets;
 }
 
 
-function createPreloadTrials(jsonData, bucketUri, lng, device) {
+export function createPreloadTrials(jsonData, bucketUri) {
   let preloadTrials = {};
+  const lng = getLanguage()
+  const device = getDevice()
 
-  // Determine top-level key: 'preload' or 'all'
   let topLevelKey = jsonData.hasOwnProperty('preload') ? 'preload' : 'all';
 
   // Iterate over groups ('preload') or asset types ('all')
-  for (let groupOrType in jsonData[topLevelKey]) {
-      let groupAssets = jsonData[topLevelKey][groupOrType];
-
-      // When top-level key is 'preload'
+  for (let key in jsonData[topLevelKey]) {
       if (topLevelKey === 'preload') {
-          preloadTrials[groupOrType] = generateTrial(groupAssets, bucketUri, lng, device);
-      }
-      // When top-level key is 'all'
-      else {
-          preloadTrials[topLevelKey] = generateTrial(groupAssets, bucketUri, lng, device);
+          let groupAssets = jsonData[topLevelKey][key];
+          preloadTrials[key] = generatePreloadTrial(groupAssets, bucketUri, lng, device);
+      } else {
+          preloadTrials[topLevelKey] = generatePreloadTrial(jsonData[topLevelKey], bucketUri, lng, device);
+          break;
       }
   }
-  console.log('preload trials: ', preloadTrials)
+
   return preloadTrials;
 }
 
-function generateTrial(groupAssets, bucketUri, lng, device) {
+function generatePreloadTrial(groupAssets, bucketUri, lng, device) {
   let trial = {
-      type: 'preload', // use actual jsPsych preload plugin type here
+      type: jsPsychPreload,
       images: [],
       audio: [],
       video: [],
@@ -152,26 +121,27 @@ function generateTrial(groupAssets, bucketUri, lng, device) {
       on_success: null
   };
 
-  // Mapping between asset type and trial key
-  const assetTypeMapping = {
-      images: 'images',
-      audio: 'audio',
-      video: 'video'
-  };
-
-  for (let assetType in groupAssets) {
-      let specificAssets = groupAssets[assetType]['languageSpecific'];
-      let sharedAssets = groupAssets[assetType]['shared'];
-
-      specificAssets.forEach(asset => {
-          trial[assetTypeMapping[assetType]].push(`${bucketUri}/${lng}/${device}/${asset}`);
-      });
-
-      sharedAssets.forEach(asset => {
-          trial[assetTypeMapping[assetType]].push(`${bucketUri}/shared/${device}/${asset}`);
-      });
-  }
+  ['languageSpecific', 'shared'].forEach(type => {
+      if (groupAssets[type]) {
+          groupAssets[type].forEach(asset => {
+              let mimeType = mime.lookup(asset);
+              if (mimeType.startsWith('image/')) {
+                  trial.images.push(`${bucketUri}/${type === 'languageSpecific' ? lng : 'shared'}/${device}/${asset}`);
+              }
+              else if (mimeType.startsWith('audio/')) {
+                  trial.audio.push(`${bucketUri}/${type === 'languageSpecific' ? lng : 'shared'}/${device}/${asset}`);
+              }
+              else if (mimeType.startsWith('video/')) {
+                  trial.video.push(`${bucketUri}/${type === 'languageSpecific' ? lng : 'shared'}/${device}/${asset}`);
+              }
+              else {
+                  throw new Error('Invalid MIME type. Only image, audio, and video file types are allowed.');
+              }
+          });
+      }
+  });
 
   return trial;
 }
+ 
 
