622e6793e32bd58930a851bad15386b4a0230ad6
Philip Ziolkowski <philipz4848@gmail.com>
2023-09-08 10:20:18 -0700

    Removing comments

--- src/trials.js
+++ src/trials.js
@@ -4,7 +4,6 @@ export const startGame = {
     type: jsPsychHtmlMultiResponse,
     stimulus: () => {
         return (
-            // String
             `<div >
                 <p style="font-size: 2rem; position:absolute; top: 35%; left: 50%; transform: translate(-50%, -50%); margin: 0; font-weight: semi-bold; color: rgba(76, 101, 139, 1);">Click here to start!</p>
             </div>`
@@ -13,17 +12,9 @@ export const startGame = {
     choices: ['Go'],
     button_html: () => {
         return (
-        // String
             `<button style="position:absolute; top: 0; bottom: 0; left: 0; right: 0; margin: auto; width: 12vw; height: 12vh; background: none; border: 0px; width: 16vw">
                 <img style="width: 15vw; cursor: pointer;" src='https://storage.googleapis.com/roar-pa/shared/go-blink.gif' alt='Go button'/>
             </button>`
         )
     }
 }
-
-// position: absolute;
-// top: 0;
-// bottom: 0;
-// left: 0;
-// right: 0;
-// margin: auto;
